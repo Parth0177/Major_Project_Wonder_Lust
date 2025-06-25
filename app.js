4ee7ca0f0f9e99d83bc3ba95d361d058e426815e
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-26 00:38:33 +0530

    refactor listing model to set default image URL for empty image field

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app= express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing');
 
 const MONGO_URL= 'mongodb://127.0.0.1:27017/WonderLust';
 async function main(){
@@ -15,6 +16,22 @@ main().then((res)=>{
 app.get('/',(req,res)=>{
   res.send('Welcome');
 });
+
+app.get('/testListing', async(req,res)=>{  
+    let listing = new Listing({
+    title: 'Beautiful Beach House',
+    description: 'A lovely beach house with stunning views.',
+    images: '',
+    price: 5250,
+    location: 'Malibu, CA',
+    country: 'USA'
+  });
+
+  await listing.save()
+    .then(() => res.send('Listing created successfully'))
+    .catch(err => res.status(500).send('Error creating listing: ' + err.message));
+    res.send('Succesfully tested');
+})
 
 app.listen(8080,()=>{
   console.log('Server is running on PORT 8080');
--- models/listing.js
+++ models/listing.js
@@ -4,12 +4,14 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
   title: {
     type:String,
+    required:true,
   },
   description:{
     type:String,
   },
   images:{
     type:String,
+    set: (v)=> v === "" ? "https://images.unsplash.com/photo-1544984243-ec57ea16fe25?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
   },
   price:{
     type:Number,
