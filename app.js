86ddd5f2baf45232577a7c2d4af126987008c74b
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-27 00:57:07 +0530

    add ejs-mate for layout and partial support; implement new listing views and update existing templates

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(methodOverride('_method')); // For supporting PUT and DELETE methods in forms
+app.engine('ejs', ejsMate);
 
 
 const MONGO_URL= 'mongodb://127.0.0.1:27017/WonderLust';
