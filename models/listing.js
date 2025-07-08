6a05572dfc55d085242d672d5ad3f75060f62c95
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-09 00:24:07 +0530

    fix: format required field in listing schema and add post-delete hook for reviews

--- models/listing.js
+++ models/listing.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
   title: {
     type: String,
-    required:true,
+    required: true,
   },
   description: String,
   image: {
@@ -24,6 +25,12 @@ const listingSchema = new Schema({
     }
   ]
 });
+
+listingSchema.post("findOneAndDelete", async (listing)=>{
+  if(listing){
+    await Review.deleteMany({_id : {$in: listing.reviews}})
+  }
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
