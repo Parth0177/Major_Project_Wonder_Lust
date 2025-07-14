5d1bbc55fe68e258c721aade277d6bd6fe49b45c
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-14 23:55:45 +0530

    feat: add user-related routes for CRUD operations

--- classroom/server.js
+++ classroom/server.js
@@ -5,7 +5,20 @@ app.get('/', (req,res)=>{
   res.send('Hello World');
 });
 
+app.get('/users', (req,res)=>{
+  res.send('Users Page it is');
+});
 
+app.get('/users/:id', (req,res)=>{
+  res.send('Show Page');
+});
+
+app.post('/users', (req,res)=>{
+  res.send('Create User');
+});
+app.delete('/users/:id', (req,res)=>{
+  res.send('delete User');
+});
 
 app.listen(3000, ()=>{
   console.log('Server is runnung on 3000')
