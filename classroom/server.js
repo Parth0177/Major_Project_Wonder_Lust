2b36de33b4ebef045ed371597f86e3d9584fdcf0
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-17 00:42:36 +0530

    feat: integrate cookie-parser middleware and update package dependencies

--- classroom/server.js
+++ classroom/server.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const app = express();
-const users= require('./routes/user.js')
-const posts= require('./routes/post.js')
+const users = require('./routes/user.js');
+const posts = require('./routes/post.js');
+const cookieParser = require('cookie-parser');
 
-app.get('/getcookies', (req,res)=>{
-  res.cookie("greet", "Hello");
-  res.cookie("greet", "namaste")
+app.use(cookieParser());
+
+app.get('/getcookies', (req, res) => {
+  res.cookie("greet_en", "Hello");
+  res.cookie("greet_hi", "Namaste");
   res.send("Send some cookies...");
 });
 
-
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
+  console.dir(req.cookies);
   res.send('Hello World');
 });
 
-app.use('/users ', users);
-app.use('/posts ', posts);
+app.use('/users', users);
+app.use('/posts', posts);
 
-
-
-
-
-
-
-app.listen(3000, ()=>{
-  console.log('Server is runnung on 3000')
+app.listen(3000, () => {
+  console.log('Server is running on 3000');
 });
