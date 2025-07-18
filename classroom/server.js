51b20d67982e9a6c7feffcc326e0da5e77ad9a12
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-18 17:02:06 +0530

    feat: add cookie management for express-session
    - Implemented a new Cookie class for managing session cookies, including properties like path, maxAge, httpOnly, and secure.
    - Added methods for serializing cookies and returning JSON representations.
    
    feat: create in-memory session store for express-session
    
    - Developed MemoryStore class to handle session storage in memory.
    - Implemented methods for session management including all, clear, destroy, get, set, length, and touch.
    
    feat: enhance session management in express-session
    
    - Created Session class to manage session data and methods for touch, resetMaxAge, save, reload, destroy, and regenerate.
    - Improved session loading and regeneration logic.
    
    feat: abstract session store functionality
    
    - Introduced Store class as an abstract base for session stores.
    - Added methods for regenerating sessions and loading session instances.
    
    chore: update on-headers module
    
    - Added new version with security fixes and improved functionality.
    - Updated README, LICENSE, and package.json files for on-headers.
    
    chore: update random-bytes module
    
    - Added new version with improved random byte generation.
    - Updated README, LICENSE, and package.json files for random-bytes.
    
    chore: update uid-safe module
    
    - Added new version with enhancements for generating URL and cookie safe UIDs.
    - Updated README, LICENSE, and package.json files for uid-safe.

--- classroom/server.js
+++ classroom/server.js
@@ -2,39 +2,14 @@ const express = require('express');
 const app = express();
 const users = require('./routes/user.js');
 const posts = require('./routes/post.js');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-app.use(cookieParser("secretcode"));
-
-app.get('/getsignedcookie', (req,res)=>{
-  res.cookie('made-in', " india", {signed:true})
-  res.send('signedCookies')
-});
-
-app.get('/verify', (req,res)=>{
-  console.log(req.signedCookies);
-  res.send("verified");
-})
+app.use(session({
+  secret
+}))
 
 
-app.get('/getcookies', (req, res) => {
-  res.cookie("greet_en", "Hello");
-  res.cookie("greet_hi", "Namaste");
-  res.send("Send some cookies...");
-});
 
-app.get('/greet', (req,res)=>{
-  let {name= "anonymous"} = req.cookies;
-  res.send(`hello guyz! myself ${name}`);
-});
-
-app.get('/', (req, res) => {
-  console.dir(req.cookies); 
-  res.send('Hello World');
-});
-
-app.use('/users', users);
-app.use('/posts', posts);
 
 app.listen(3000, () => {
   console.log('Server is running on 3000');
