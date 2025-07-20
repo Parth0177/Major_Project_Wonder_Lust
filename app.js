3f4d6fbfb9b91ad2c2fe5c8e9aa0eb1d5de9629b
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-21 00:24:54 +0530

    feat: add session management with express-session middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const Review = require('./models/review');
+const session = require('express-session');
 
 
 app.set('view engine', 'ejs');
@@ -14,6 +15,14 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(methodOverride('_method')); // For supporting PUT and DELETE methods in forms
 app.engine('ejs', ejsMate);
+
+const sessionOptions= {
+  secret: 'mySuperSecretCode',
+  resave:false,
+  saveUninitialized: true,
+};
+
+app.use(session(sessionOptions));
 
 
 const MONGO_URL= 'mongodb://127.0.0.1:27017/WonderLust';
