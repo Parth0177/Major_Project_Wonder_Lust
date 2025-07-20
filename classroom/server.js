6e5c9c34d0db61dfcec5d0820d458780b1d7c865
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-20 23:40:54 +0530

    feat: add connect-flash middleware to server.js

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,7 @@ const app = express();
 const users = require('./routes/user.js');
 const posts = require('./routes/post.js');
 const session = require('express-session');
+const flash = require('conect-flash');
 
 app.use(session({
   secret: "MySuperSecretString",
