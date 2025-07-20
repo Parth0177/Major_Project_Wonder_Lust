2cafd29d7c753566ee0ae7fd497846f982b6d8db
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-21 00:34:34 +0530

    feat: enhance session management with cookie options for security

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ const sessionOptions= {
   secret: 'mySuperSecretCode',
   resave:false,
   saveUninitialized: true,
+  cookie:{
+    expires: Date.now()+ 7*24*60*60*1000, // 7 days
+    maxAge: 7*24*60*60*1000, // 7 days
+    httpOnly: true, // Helps prevent XSS attacks
+  }
 };
 
 app.use(session(sessionOptions));
