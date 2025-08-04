a277eb883a4839562da8f8319f265c31c2046af9
Parth Tiwari <parthtiwari670@gmail.com>
2025-08-05 01:19:54 +0530

    kk

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const LocalStrategy = require('passport-local');
 const { isLoggedIn, isOwner, isAuthor } = require('./middleware'); 
 const {saveRedirectUrl} = require('./middleware');
 const listingController = require('./controller/listings');
+const reviewController = require('./controller/reviews');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -106,19 +107,19 @@ app.get('/logout', (req,res)=>{
   })
 })
 
+// Error handling middleware
+app.use((err , req, res , next)=>{
+  res.send('Something went wrong');
+});
+
+
 app.get('/',(req,res)=>{
 res.render('home.ejs');
 });
 
+
 // Index Route
 app.get('/listings',(listingController.index));
-
-
-// Error handling middleware
-app.use((err , req, res , next)=>{
-  res.send('Something went wrong');
-})
-
 
 
 //NEW ROUTE
@@ -140,28 +141,11 @@ app.put('/listings/:id', isLoggedIn, isOwner , (listingController.update));
 app.delete('/listings/:id', isLoggedIn,isOwner, (listingController.delete));
 
 // Reviews // POST Route
-app.post('/listings/:id/reviews',isLoggedIn, async (req,res)=>{
-  let listing = await Listing.findById(req.params.id)
-  let newReview = new Review(req.body.review);
-  newReview.author = req.user._id; // Set the author to the currently logged-in user
-
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-  req.flash('success', 'Review added successfully!');
-  res.redirect(`/listings/${listing._id}`);
-});
+app.post('/listings/:id/reviews',isLoggedIn,(reviewController.createReviews) );
 
 // DELETE REVIEW ROUTE
 
-app.delete('/listings/:id/reviews/:reviewId', isLoggedIn, isAuthor, async (req, res) => {
-  const { id, reviewId } = req.params;
-
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  req.flash('success', 'Review deleted successfully!');
-  res.redirect(`/listings/${id}`);
-});
+app.delete('/listings/:id/reviews/:reviewId', isLoggedIn, isAuthor, (reviewController.deleteReviews));
 
 
 
---
+++ controller/reviews.js
@@ -0,0 +1,23 @@
+const Listing = require('../models/listing');
+const Review = require('./models/review');
+
+module.exports.createReviews = async (req,res)=>{
+  let listing = await Listing.findById(req.params.id)
+  let newReview = new Review(req.body.review);
+  newReview.author = req.user._id; // Set the author to the currently logged-in user
+
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  req.flash('success', 'Review added successfully!');
+  res.redirect(`/listings/${listing._id}`);
+};
+
+module.exports.deleteReviews = async (req, res) => {
+  const { id, reviewId } = req.params;
+
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash('success', 'Review deleted successfully!');
+  res.redirect(`/listings/${id}`);
+}
